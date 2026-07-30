bd105272b42fd29f5e2def60eed86134ca95ccf1
~/cat/em1/nal/ <emaperry@stupidcat.uk>
2026-07-30 13:38:15 +0100

    Support for Criminal Dusk

--- Various Text Changes/vtc.lua
+++ Various Text Changes/vtc.lua
@@ -1,43 +1,44 @@
+if Global.CrimDusk then return end
+-- Criminal Dusk contains its own version of VTC, abort
+
 local vtc_options = io.load_as_json(SavePath .. "vtc.txt") or {
-	vtc_main_menu = true,
-	vtc_crimenet = true,
-	vtc_preplanning = true,
-	vtc_gameplay = true,
-	vtc_discord = true
+  vtc_main_menu = true,
+  vtc_crimenet = true,
+  vtc_preplanning = true,
+  vtc_gameplay = true,
+  vtc_discord = true
 }
 
 local VTCPath = ModPath
--- I don't know why I need this but BeardLib is overwriting ModPath inside
--- the LocalizationManagerPostInit hook, causing the mod to fail to load.
 
 Hooks:Add("MenuManagerBuildCustomMenus", "vtc_menumanager", function()
-	function MenuCallbackHandler:vtc_save_json(item)
-		vtc_options[item:name()] = item:value() == "on"
-		io.save_as_json(vtc_options, SavePath .. "vtc.txt")
-	end
+  function MenuCallbackHandler:vtc_save_json(item)
+    vtc_options[item:name()] = item:value() == "on"
+    io.save_as_json(vtc_options, SavePath .. "vtc.txt")
+  end
 end)
 
 Hooks:Add("LocalizationManagerPostInit", "vtc_loc", function(loc)
-	loc:load_localization_file(VTCPath .. "loc/fixes.json")
-	if vtc_options.vtc_main_menu then
-		loc:load_localization_file(VTCPath .. "loc/menu.json")
-		if not CrimDawn then
-		  loc:load_localization_file(VTCPath .. "loc/cd_compat/menu.json")
-		end
-	end
-	if vtc_options.vtc_crimenet then
-		loc:load_localization_file(VTCPath .. "loc/crimenet.json")
-	end
-	if vtc_options.vtc_preplanning then
-		loc:load_localization_file(VTCPath .. "loc/preplanning.json")
-	end
-	if vtc_options.vtc_gameplay then
-		loc:load_localization_file(VTCPath .. "loc/gameplay.json")
-		loc:load_localization_file(VTCPath .. "loc/hints.json")
-	end
-	if vtc_options.vtc_discord then
-		loc:load_localization_file(VTCPath .. "loc/discord.json")
-	end
+  loc:load_localization_file(VTCPath .. "loc/fixes.json")
+  if vtc_options.vtc_main_menu then
+    loc:load_localization_file(VTCPath .. "loc/menu.json")
+    if not CrimDawn then
+      loc:load_localization_file(VTCPath .. "loc/cd_compat/menu.json")
+    end
+  end
+  if vtc_options.vtc_crimenet then
+    loc:load_localization_file(VTCPath .. "loc/crimenet.json")
+  end
+  if vtc_options.vtc_preplanning then
+    loc:load_localization_file(VTCPath .. "loc/preplanning.json")
+  end
+  if vtc_options.vtc_gameplay then
+    loc:load_localization_file(VTCPath .. "loc/gameplay.json")
+    loc:load_localization_file(VTCPath .. "loc/hints.json")
+  end
+  if vtc_options.vtc_discord then
+    loc:load_localization_file(VTCPath .. "loc/discord.json")
+  end
 end)
 
 MenuHelper:LoadFromJsonFile(VTCPath .. "vtc_menu.txt", nil, vtc_options)
